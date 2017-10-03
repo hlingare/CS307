463b2eee6ee961f7e88f1101cb0cbba04a837f87
Vijay <vviswan@purdue.edu>
2017-10-02 22:20:47 -0400

    flask application update

--- myflaskapp.py
+++ myflaskapp.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def display():
-    return "Gokul made it work"
+    return "Flask application properly functioning"
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', debug=True, port=3134)
