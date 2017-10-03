e3f0311375745dd5beaa37feb6c0fac8ffc28be4
sanatmouli <sanatmouli@gmail.com>
2017-10-02 21:57:07 -0400

    created a student_course table which establishes a many to many relationship between student and course table. Also created test data and 3 queries to test relationships

--- test.sql
+++ test.sql
@@ -29,3 +29,38 @@ create table course (
        department varchar(100),
        degree ENUM('BS','MS','PhD')
 );
+#
+# add courses to a course table
+#
+
+insert into `course` (`id`, `name`, `cid`, `department`, `degree`) values (null, "CS", "25000", "Computer Science", "BS");
+
+insert into `course` (`id`, `name`, `cid`, `department`, `degree`) values (null, "CS", "25100", "Computer Science", "BS");
+
+insert into `course` (`id`, `name`, `cid`, `department`, `degree`) values (null, "CS", "25200", "Computer Science", "BS");
+
+insert into `course` (`id`, `name`, `cid`, `department`, `degree`) values (null, "MGMT", "20100","Management", "BS");
+
+insert into `course` (`id`, `name`, `cid`, `department`, `degree`) values (null, "EAPS", "30100","Earth and Planetary Sciences", "BS");
+
+
+
+create table student_course (
+       id int not null primary key auto_increment, 
+       studentID int,
+       courseID int,
+       foreign key(studentID) references student(id),
+       foreign key(courseID) references course(id)
+);
+insert into `student_course` (`id`, `studentID`, `courseID`) values (null, 1, 1);
+insert into `student_course` (`id`, `studentID`, `courseID`) values (null, 1, 2);
+insert into `student_course` (`id`, `studentID`, `courseID`) values (null, 1, 3);
+insert into `student_course` (`id`, `studentID`, `courseID`) values (null, 2, 1);
+insert into `student_course` (`id`, `studentID`, `courseID`) values (null, 2, 3);
+insert into `student_course` (`id`, `studentID`, `courseID`) values (null, 3, 3);
+insert into `student_course` (`id`, `studentID`, `courseID`) values (null, 3, 2);
+insert into `student_course` (`id`, `studentID`, `courseID`) values (null, 4, 1);
+insert into `student_course` (`id`, `studentID`, `courseID`) values (null, 4, 3);
+insert into `student_course` (`id`, `studentID`, `courseID`) values (null, 5, 4); 
+
+select a.name, a.puid, a.standing, a.degree, a.major, b.name, b.cid, b.department, b.degree from student a, course b, student_course c where c.studentID=a.id and c.courseID=b.id;
