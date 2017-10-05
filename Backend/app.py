b6131fa08541f134718da304a7603caf4e0a2d06
GokulsreeY <Siri@Gokulsrees-MacBook-Pro.local>
2017-10-05 04:32:47 -0400

    added ml service

--- Backend/app.py
+++ Backend/app.py
@@ -2,6 +2,10 @@
 from flask import Flask,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+from database_connector import read
+from machine_learning_service import ml_train, ml_predict, normalize
+from sklearn import neighbors
+import psycopg2
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yipgikbasudyog:21d1ee6803375e19da2ed3cfc8c726f036e3e11871d62b65df13134be5c69ec2@ec2-54-163-229-169.compute-1.amazonaws.com:5432/df5g8vla4snv52'
@@ -27,6 +31,46 @@ def prereg():
       response = jsonify(results)
       response.status_code = 200
       return response
+
+@app.route('/result_list')
+def result_list():
+    con = None
+    try:
+        con = psycopg2.connect(host = 'ec2-54-163-229-169.compute-1.amazonaws.com', database = 'df5g8vla4snv52', user = 'yipgikbasudyog', password = '21d1ee6803375e19da2ed3cfc8c726f036e3e11871d62b65df13134be5c69ec2')
+        cur = con.cursor()
+        cur.execute("SELECT * FROM courseList")
+        colnames = [desc[0] for desc in cur.description]
+        rows = cur.fetchall()
+        temp = []
+        for row in rows:
+            temp.append(list(row))
+
+        courses = []
+        for i in range(0, len(temp)):
+            courses.append(temp[i][1])
+
+        n_neighbors = 4
+        clf = neighbors.KNeighborsClassifier(n_neighbors, weights='uniform')
+        train_data = read(0)
+        pred_data = read(1)
+        ml_train(train_data, clf)
+        predicts = ml_predict(pred_data, clf)
+        fin_pred = []
+        for i in range(0, len(predicts)):
+            if (predicts[i] == 0):
+                string = "Yes: " + courses[i]
+                fin_pred.append(string)
+            else:
+                string = "No: " + courses[i]
+                fin_pred.append(string)
+
+        response = jsonify(fin_pred)
+        response.status_code = 200
+        return response
+    finally:
+        if con:
+            con.close()
+
 
 if __name__ == '__main__':
           port = 8000 #the custom port you want
---
+++ Backend/machine_learning_service.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 29 11:23:47 2017
+
+@author: Vishaal Bommena
+"""
+import numpy as np
+from random import randint
+from sklearn import neighbors
+
+def normalize(X, Y):
+    for i in range (0, len(Y)):
+        if (Y[i]) == 11:
+            for j in range(0, len(X[i])):
+                X[i][j] = X[i][j] - 4
+        if (Y[i]) == 12:
+            for j in range(0, len(X[i])):
+                X[i][j] = X[i][j] - 2
+        if (Y[i]) == 13:
+            for j in range(0, len(X[i])):
+                X[i][j] = X[i][j] - 0   
+    return X
+
+def ml_train(X, clf):
+    #X = normalize(X, option)
+    #Training Session
+    y= []
+    train = []
+    X1 = []
+    X2 = []
+    for i in range (0, len(X)):
+        if (i < (len(X) / 2)):
+            X1.append(X[i])
+        else: 
+            X2.append(X[i])
+    y_0 = run_avg(X1, 0)
+    y_1 = run_avg(X2, 1)
+    train = X1 + X2
+    y=y_0 + y_1 
+    clf.fit(train, y) 
+    print("Training Complete")
+
+def ml_predict(X, clf):
+#    print(clf)
+    prediction = clf.predict(X)
+    return prediction
+
+def run_avg(X, a):
+    y = []
+    size = len(X)
+    for i in range(0, size):
+        y.append(a)
+    return y
