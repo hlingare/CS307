347310812f98006a448907362815d08b828522cd
sanatmouli <sanatmouli@gmail.com>
2017-10-07 21:08:04 -0400

    trying to add ml service

--- app.py
+++ app.py
@@ -2,12 +2,14 @@
 from flask import Flask,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+from flask_cors import CORS
 from database_connector import read
 from machine_learning_service import ml_train, ml_predict, normalize
 from sklearn import neighbors
 import psycopg2
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yipgikbasudyog:21d1ee6803375e19da2ed3cfc8c726f036e3e11871d62b65df13134be5c69ec2@ec2-54-163-229-169.compute-1.amazonaws.com:5432/df5g8vla4snv52'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -74,4 +76,5 @@ def result_list():
 
 if __name__ == '__main__':
           port = 8000 #the custom port you want
-          app.run(host='0.0.0.0', port=port)
+          app.run(debug=True)
+          #app.run(host='0.0.0.0', port=port)
