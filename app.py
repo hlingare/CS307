df136f387873ad7c4b117ee5ede3f80ca164bd75
sanatmouli <sanatmouli@gmail.com>
2017-10-25 14:23:03 -0400

    route takes a string of course information as param

--- app.py
+++ app.py
@@ -73,6 +73,50 @@ def result_list():
         if con:
             con.close()
 
+@app.route('/showStudent/<text>')
+def studentinfo(text):
+    con = None
+    try:
+        con = psycopg2.connect(host = 'ec2-54-163-229-169.compute-1.amazonaws.com', database = 'df5g8vla4snv52', user = 'yipgikbasudyog', password = '21d1ee6803375e19da2ed3cfc8c726f036e3e11871d62b65df13134be5c69ec2')
+        cur = con.cursor()
+        #print(text)
+        #cur.execute("DROP TABLE IF EXISTS student")
+        #cur.execute("CREATE TABLE student(Id Int PRIMARY KEY,username text, name text, taken_course text, option text, recommended text, uuid Int, courseid Int)")
+        #words = text.split()
+        #words = text.read().split(',')
+        words = text.split(",")
+        #for word in words:
+        #    print word
+        #idstudent = 0
+        #username = "smouli"
+        #name = "Sanat"
+        #taken_course = "cs250"
+        #option = "0"
+        #uuid = 007
+        #courseid = 90
+        #query = ("INSERT INTO student(id, username, name, taken_course, recommended, uuid, courseid) VALUES (%s, %s, %s, %s, %s, %s, %s)")
+        #cur.execute(query, (idstudent, username, name, course, option, uuid, courseid,))
+        #con.commit()
+        # int text text text text int int
+        #idstudent = words[0]
+        usrname = words[0]
+        name = words[1]
+        taken_course = words[2]
+        option = words[3]
+        recommended = words[4]
+        uuid = words[5]
+        courseid = words[6]
+        print courseid
+        print usrname
+        print type(uuid)
+        #query = ("INSERT INTO student(username, name, taken_course, recommended, uuid, courseid) VALUES (%s, %s, %s, %s, %s, %s)")
+        #cur.execute(query, usrname, words[1], words[2], words[3], words[4], words[5])
+        query = "INSERT INTO student (username, name, taken_course, option, recommended, uuid, courseid) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        cur.execute(query, (usrname, name, taken_course, option, recommended, uuid, courseid))
+        con.commit()
+    finally:
+        if con:
+            con.close()
 
 if __name__ == '__main__':
           port = 8000 #the custom port you want
