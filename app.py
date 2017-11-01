2c2545f70ea2c68a9666349a7b5faf367123fa71
sanatmouli <sanatmouli@gmail.com>
2017-11-01 19:30:38 -0400

    committing for gokul

--- app.py
+++ app.py
@@ -152,9 +152,12 @@ def updateUsername():
         con.commit()
         response.status_code = 200
         return response
+    finally:
+        if con:
+            con.close()
 
 @app.route('/getCourse', methods = ['GET'])
-def course():
+def getCourse():
     con = None
     try:
         con = psycopg2.connect(host = 'ec2-54-163-229-169.compute-1.amazonaws.com', database = 'df5g8vla4snv52', user = 'yipgikbasudyog', password = '21d1ee6803375e19da2ed3cfc8c726f036e3e11871d62b65df13134be5c69ec2')
