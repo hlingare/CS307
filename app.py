f3a419e6dee12d03696f622f0a9890a25ec7b041
GokulsreeY <Siri@Gokulsrees-MacBook-Pro.local>
2017-11-03 00:59:40 -0400

    Added post get for rec courses with scores

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 from flask_cors import CORS
-from database_connector import read
-from machine_learning_service import ml_train, ml_predict, normalize
+from database_connector import read, course_list, training_data, training, predicts, create_uid, predict_data
+from machine_learning_service import ml_train, normalize
 from sklearn import neighbors
 import psycopg2
 from flask import request
@@ -38,44 +38,64 @@ def prereg():
       response.status_code = 200
       return response
 
-@app.route('/result_list')
+@app.route('/result_list', methods=['POST'])
 def result_list():
     con = None
     try:
         con = psycopg2.connect(host = 'ec2-54-163-229-169.compute-1.amazonaws.com', database = 'df5g8vla4snv52', user = 'yipgikbasudyog', password = '21d1ee6803375e19da2ed3cfc8c726f036e3e11871d62b65df13134be5c69ec2')
         cur = con.cursor()
-        cur.execute("SELECT * FROM courseList")
-        colnames = [desc[0] for desc in cur.description]
-        rows = cur.fetchall()
-        temp = []
-        for row in rows:
-            temp.append(list(row))
+        #words = text.split(",")
+        text = request.data
+        print("TExt:",text)
+        dart = json.loads(text)
+        uid = dart["userId"]
+        print(uid)
+        #uid = words[0]
+        currentUid = (uid,)
+        uid = str(uid)
 
-        courses = []
-        for i in range(0, len(temp)):
-            courses.append(temp[i][1])
-
-        n_neighbors = 4
-        clf = neighbors.KNeighborsClassifier(n_neighbors, weights='uniform')
-        train_data = read(0)
-        pred_data = read(1)
-        ml_train(train_data, clf)
-        predicts = ml_predict(pred_data, clf)
-        fin_pred = []
-        for i in range(0, len(predicts)):
-            if (predicts[i] == 0):
-                string = "Yes: " + courses[i]
-                fin_pred.append(string)
-            else:
-                string = "No: " + courses[i]
-                fin_pred.append(string)
-
-        response = jsonify(fin_pred)
-        response.status_code = 200
-        return response
+        n_neighbors = 1
+        clf = neighbors.KNeighborsClassifier(n_neighbors, weights = 'uniform')
+        courseList, options = course_list(uid)
+        data_train = training_data(courseList)
+        norm_data = normalize(data_train,options)
+        training(norm_data, clf)
+        pred = predict_data()
+        distances = predicts(pred, clf)
+        create_uid(uid, distances)
+        return distances
     finally:
         if con:
             con.close()
+@app.route('/get_result_list',methods=['GET'])
+def get_result_list():
+    con = None
+    try:
+        con = psycopg2.connect(host = 'ec2-54-163-229-169.compute-1.amazonaws.com', database = 'df5g8vla4snv52', user = 'yipgikbasudyog', password = '21d1ee6803375e19da2ed3cfc8c726f036e3e11871d62b65df13134be5c69ec2')
+        cur = con.cursor()
+        text = request.args.get('userId')
+        print("Text:",text)
+        #dart = json.loads(text)
+        #uid = dart["userId"]
+        currentUid = (text,)
+        cur.execute('SELECT * FROM "{}"'.format(str(text)))
+        rows = cur.fetchall()
+        result = []
+        for row in rows:
+             #print(row,"row")
+             obj = {
+               'id': row[0],
+               'name': row[1],
+               'score': row[2],
+             }
+             result.append(obj)
+             print("OBJ: ", obj)
+             response = jsonify(result)
+             response.status_code = 200
+        return response
+    finally:
+         if con:
+             con.close()
 
 @app.route('/showStudent', methods=['POST'])
 def studentinfo():
